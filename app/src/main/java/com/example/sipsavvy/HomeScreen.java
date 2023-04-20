3d72fedb16e3b68121358cfd4e17b78c33e3bd37
Laura Leal <laura.leal05@utrgv.edu>
2023-04-20 01:18:11 -0500

    progress bar working w button, just need to make it work when pressing enter for custom amount

--- app/src/main/java/com/example/sipsavvy/HomeScreen.java
+++ app/src/main/java/com/example/sipsavvy/HomeScreen.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.*;
 import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity {
 
+    private int progress = 0;
+
+    private ProgressBar progressBar;
+    private TextView textViewProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +38,58 @@ public class HomeScreen extends AppCompatActivity {
         TextView idealWaterIntake2 = findViewById(R.id.idealWaterIntake2);
         int goal = (waterIntake * 20) / 100;
         int finalGoal = waterIntake - goal;
-        idealWaterIntake2.setText(finalGoal + " ounces is your current goal! You got this!");
+        idealWaterIntake2.setText(finalGoal + " ounces is your current goal!");
+
+
+        //for progress bar
+        progressBar = findViewById(R.id.progress_bar);
+        textViewProgress = findViewById(R.id.text_view_progress);
+        progressBar.setProgress(0);
+        textViewProgress.setText("0%");
+
+        EditText customAmount = findViewById(R.id.customAmount);
+      //  String amount = customAmount.getText().toString();
+      //  int intAmount = Integer.valueOf(amount);
+
+
+        //fix this
+        customAmount.setOnEditorActionListener(
+                (arg0, actionId, event) -> {
+                    if (actionId == EditorInfo.IME_ACTION_DONE)
+                    {
+                        progress += 10;
+                        updateProgressBar();
+                    }
+                    return false;
+                });
+        //
+
+
+        Button addEightOunces = findViewById(R.id.addEightOunces);
+        addEightOunces.setOnClickListener(v ->
+        {
+            if(progress <= finalGoal)
+            {
+                progress += 8;
+                updateProgressBar();
+            }
+
+            if(progress >= finalGoal)
+            {
+                updateProgressBar();
+                textViewProgress.setText("You did it! Congratulations!");
+            }
+        });
 
     }
+
+
+
+    private void updateProgressBar()
+    {
+        progressBar.setProgress(progress);
+        textViewProgress.setText(progress + "%");
+    }
+
 
 }
