43cece7bb0d2d1dab7b08d8b86c9ccbe7f03cdfa
Laura Leal <laura.leal05@utrgv.edu>
2023-04-19 21:35:02 -0500

    some home screen done.

--- app/src/main/java/com/example/sipsavvy/MainActivity.java
+++ app/src/main/java/com/example/sipsavvy/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         },3000);
+
 
 
 
--- app/src/main/java/com/example/sipsavvy/Screen1.java
+++ app/src/main/java/com/example/sipsavvy/Screen1.java
@@ -2,13 +2,34 @@ package com.example.sipsavvy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Screen1 extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen1);
+
+        EditText UserName = findViewById(R.id.UserName);
+        EditText UserAge = findViewById(R.id.UserAge);
+        EditText UserWeight = findViewById(R.id.UserWeight);
+
+        Button submitButton = findViewById(R.id.submitButton);
+
+        Intent secondIntent = new Intent(this, HomeScreen.class);
+
+        submitButton.setOnClickListener(v ->
+        {
+            secondIntent.putExtra("com.example.SipSavvy.userInput", UserName.getText().toString());
+            secondIntent.putExtra("com.example.SipSavvy.userInput2", UserAge.getText().toString());
+            secondIntent.putExtra("com.example.SipSavvy.userInput3", UserWeight.getText().toString());
+
+            startActivity(secondIntent);
+        });
     }
 }
